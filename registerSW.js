3580a06569f4bde2c67583830917f27fff35d88d
haseebn19 <haseeb.kn@outlook.com>
2026-04-23 06:52:36 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/requizle-web/sw.js', { scope: '/requizle-web/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
